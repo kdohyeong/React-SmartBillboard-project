d5b846d90822227187356aefcde17fc6a663a1a4
unknown <do5568@naver.com>
2020-06-30 02:46:26 +0900

    Current location

--- client/src/Components/ShowMap.js
+++ client/src/Components/ShowMap.js
@@ -5,11 +5,19 @@ import './ShowMap.css';
 class ShowMap extends React.Component {
     
     componentDidMount() {
-        company__Map();
-        window.addEventListener('resize', function() { company__Map(); }, false);
+        const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+        const mapOption = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            level: 4 // 지도의 확대 레벨
+        };
+        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+        const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+        mapAround(map);
+        window.addEventListener('resize', function() { mapAround(map); }, false);
     }
     componentWillUnmount() {
-        window.removeEventListener('resize', company__Map, false);
+        window.removeEventListener('resize', mapAround, false);
     }
     render() {
     
@@ -29,37 +37,31 @@ class ShowMap extends React.Component {
   
 export default ShowMap;
 
-function company__Map() {
-    const mapContainer = document.getElementById('map'); // 지도를 표시할 div
-        const mapOption = {
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-            level: 4 // 지도의 확대 레벨
-        };
+function mapAround(map) {
+    if (navigator.geolocation) { // GPS를 지원하면
+        navigator.geolocation.getCurrentPosition(function(position) {
+        const coords = new window.kakao.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-        // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-        const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-        const geocoder = new window.kakao.maps.services.Geocoder();
-
-        geocoder.addressSearch('충청북도 청주시 서원구 충대로1', function(result, status) {
-
-            // 정상적으로 검색이 완료됐으면
-             if (status === window.kakao.maps.services.Status.OK) {
-
-                const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
-
-                // 결과값으로 받은 위치를 마커로 표시합니다
-                const marker = new window.kakao.maps.Marker({
-                    map: map,
-                    position: coords
-                });
-                // 인포윈도우로 장소에 대한 설명을 표시합니다
-                const infowindow = new window.kakao.maps.InfoWindow({
-                    content: '<div style="width:150px;text-align:center;padding:6px 0;">현재 위치</div>'
-                });
-                infowindow.open(map, marker);
-                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                map.setCenter(coords);
-            }
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        const marker = new window.kakao.maps.Marker({
+            map: map,
+            position: coords,
         });
+        // 인포윈도우로 장소에 대한 설명을 표시합니다
+        const infowindow = new window.kakao.maps.InfoWindow({
+            content: '<div style="width:150px;text-align:center;padding:6px 0;">현재 위치</div>'
+        });
+        infowindow.open(map, marker);
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+        }, function(error) {
+        console.error(error);
+        }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: Infinity
+        });
+    } else {
+        alert('GPS를 지원하지 않습니다');
+    }
 }
